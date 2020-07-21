b79fe61ad98c9399a1101e58019231de523607d8
Shaform <shaform@gmail.com>
2020-07-21 16:14:34 -0400

    add running scripts

---
+++ scripts/coco-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python3 -m scratchgan.experiment \
+  --mode="evaluate_pair" \
+  --dataset=coco-prep \
+  --data_dir=data/coco-prep \
+  --checkpoint_dir=data/coco-prep/checkpoints/ \
+  --batch_size=500 \
+  --num_examples_for_eval=500
---
+++ scripts/emnlp2017-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 -m scratchgan.experiment \
+  --mode="evaluate_pair" \
+  --dataset=emnlp2017-prep \
+  --data_dir=data/emnlp2017-prep \
+  --checkpoint_dir=data/emnlp2017-prep/checkpoints/
